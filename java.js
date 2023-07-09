1c2ae75b146c15d6ce591af44107ebfea87fec85
ducthanh20 <138018904+ducthanh20@users.noreply.github.com>
2023-07-09 18:43:52 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,28 @@
+var point =5
+if (point < 5)
+{ console.log("hoc luc kem")}
+else if( point >= 5 && point < 6.5) 
+{console.log("hoc luc trung binh")}
+else if (point >= 6.5&& point < 8)
+{console.log("hoc luc gioi")}
+else if (point >= 8)
+{console.log("hoc luc xuat xac")}
+
+var animals = ["dog","cat","bird"]
+animals.push("duck","panda","lion")
+animals.pop()
+console.log(animals)
+console.log(animals.length-1)
+animals[0]= "tiger"
+console.log(animals)
+
+var a = -2;
+var b =['1','2','3'];
+var c = 5;
+var d = a + c;
+var e = a % c;
+var f = a * c;
+
+
+
+
